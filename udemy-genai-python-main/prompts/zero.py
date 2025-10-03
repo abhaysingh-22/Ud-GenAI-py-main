82e8213efe8964ae197439d48eeac390e60e2652
abhaysingh-22 <abhaysingh6575@gmail.com>
2025-10-03 21:59:32 +0530

    fix: update API key and add additional user query in zero.py

--- udemy-genai-python-main/prompts/zero.py
+++ udemy-genai-python-main/prompts/zero.py
@@ -5,7 +5,7 @@ from openai import OpenAI
 load_dotenv()
 
 client = OpenAI(
-    api_key="AIzaSyBjA34ENgeGNplvIqCP-qcH2fuMkqxdO7o",
+    api_key="AIzaSyCyuvn83UO2cPY7T6aH74qpiwI8oA6uBSc",
     base_url="https://generativelanguage.googleapis.com/v1beta/"
 )
 
@@ -16,7 +16,8 @@ response = client.chat.completions.create(
     model="gemini-2.5-flash",
     messages=[
         { "role": "system", "content": SYSTEM_PROMPT },
-        { "role": "user", "content": "Hey, Can you write a python code to translate the word hello to Hindi"}
+        { "role": "user", "content": "Hey, Can you write a python code to translate the word hello to Hindi"},
+        { "role": "user", "content": "Also, can you tell me the capital of India?"}
     ]
 )
 
