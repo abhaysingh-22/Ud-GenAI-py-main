db37fb1035e4bca539b08aaf5e98e6e7c2abf85e
abhaysingh-22 <abhaysingh6575@gmail.com>
2025-10-03 19:48:00 +0530

    fix: update API key and modify content query in gemini_hello.py

--- udemy-genai-python-main/hello_world/gemini_hello.py
+++ udemy-genai-python-main/hello_world/gemini_hello.py
@@ -1,11 +1,11 @@
 from google import genai
 
 client = genai.Client(
-    api_key="AIzaSyBjA34ENgeGNplvIqCP-qcH2fuMkqxdO7o"
+    api_key="AIzaSyCyuvn83UO2cPY7T6aH74qpiwI8oA6uBSc"
 )
 
 response = client.models.generate_content(
-    model="gemini-2.5-flash", contents="Explain how AI works in a few words"
+    model="gemini-2.5-flash", contents="tell me the places in pune which i can visit?"
 )
 
 print(response.text)
