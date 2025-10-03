8e744c02fbd3b4bea3cabdcbbf59f65c582a493a
abhaysingh-22 <abhaysingh6575@gmail.com>
2025-10-03 19:39:20 +0530

    fix: update OpenAI client initialization and enhance chat message content

--- udemy-genai-python-main/hello_world/main.py
+++ udemy-genai-python-main/hello_world/main.py
@@ -1,14 +1,22 @@
 from dotenv import load_dotenv
 from openai import OpenAI
+import os
 
-load_dotenv()
+load_dotenv()    # Load environment variables from a .env file means it help to read the .env file and set the environment variables
 
-client = OpenAI()
+client = OpenAI(
+    base_url="https://openrouter.ai/api/v1",
+    api_key=os.getenv("OPENROUTER_API_KEY"),
+)
 
 response = client.chat.completions.create(
-    model="gpt-4o-mini",
+    extra_headers={
+        "HTTP-Referer": "https://your-site.com",  # Optional
+        "X-Title": "Your App Name",  # Optional
+    },
+    model="openai/gpt-4o",
     messages=[
-        { "role": "user", "content": "Hey, I am Piyush Garg! Nice to meet you"}
+        { "role": "user", "content": "Hey, I am Piyush Garg! Nice to meet you! tell me the places in pune where i can visit?" }
     ]
 )
 
